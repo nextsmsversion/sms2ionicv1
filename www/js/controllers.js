f6d50d55375245710d2458808a68a18a72d5a9e7
sampsontan <sampsontan@gmail.com>
2018-02-28 02:53:53 +0000

    v1.2 add Messages

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -23,23 +23,27 @@ function ($scope, $stateParams) {
 	    //Client:		https://sms2ionic-sampsontan.c9users.io 
 	    //Server:   	https://smscrowv1-sampsontan.c9users.io
 		//TODO how to make this to happen
+			$scope.msgs = {"3":"Reduced service due to incident","2":"Train with brake hang-on is approaching","1":"Last Train to"}
 			
-						
 			//alert('controller after setting scope names');
 			Friends.all($scope, $http)
 			.success(
 						function(response) {	//** pass parameter her
-							alert('SUCCESS'); 
-							$scope.names = response;
+							//tmp debugging alert('SUCCESS'); 
+							$scope.msgTypes = response.msgType;
+							$scope.msgs = response.messages;
 						}
 				)
 			.error(
 						function(msg, code) {	//** pass parameter her
 							alert('WHY $http.get returns msg: <'+msg + '> code: '+code + "???"); 
-							$scope.names = msg;	//20180226 with 407?? thanks GOD
+							$scope.msgTypes = msg.msgType;	//20180226 with 407 in OA workstation?? thanks GOD
+							$scope.msgs = msg.messages;
 						}
 			);
-})
+			
+		}
+)
    
 .controller('eDITCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
